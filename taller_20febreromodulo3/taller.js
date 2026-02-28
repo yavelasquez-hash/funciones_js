cebdccefaea5f9f495fd18dff6a4630ad763966a
yilmar andres velasquez arias <yilmarandresvelasquezarias@MacBook-Air-de-yilmar.local>
2026-02-27 22:47:33 -0500

    entrega final

--- taller_20febreromodulo3/taller.js
+++ taller_20febreromodulo3/taller.js
@@ -16,7 +16,7 @@ if (number < 0) {
 } 
 console.log("El numero es cero")
 
-// Ejercicio 2
+// Ejercicio 2......................
 
 // for (let i = 1; i<= 20; i++) {
 //     console.log(`Numero: ${i} `);
